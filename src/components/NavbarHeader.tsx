45a2152ef2e5230fabb8bc93b3df8b362fa082f6
Elin Sopiah <gitgit367@gmail.com>
2025-05-02 21:59:27 +0700

    feat: terjemah ayat

--- src/components/NavbarHeader.tsx
+++ src/components/NavbarHeader.tsx
@@ -21,7 +21,7 @@ export default function NavbarHeader({ bgClass = "bg-transparent" }) {
         <header className={`absolute inset-x-0 top-0 z-50 ${bgClass}`}>
             <nav aria-label="Global" className="flex items-center justify-between p-6 sm:p-10 lg:px-20">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
+                    <a href="/" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
                         <img
                             alt=""
@@ -57,11 +57,11 @@ export default function NavbarHeader({ bgClass = "bg-transparent" }) {
                 <div className="fixed inset-0 z-50" />
                 <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <a href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 alt=""
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                                src={logo}
                                 className="h-8 w-auto"
                             />
                         </a>
--- src/pages/Alquran/SurahDetail.tsx
+++ src/pages/Alquran/SurahDetail.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-import type { SurahDetail } from "../../types/al-quran"
+import type { SurahDetail, Ayah } from "../../types/al-quran"
+
+import NavbarHeader from "../../components/NavbarHeader"
+import Footer from "../../components/Footer"
 
 export default function SurahDetail() {
     const { id } = useParams(); // Ambil ID dari URL
     const [surah, setSurah] = useState<SurahDetail | null>(null);
-
+    const [translation, setTranslation] = useState<Ayah[]>([]);
 
     const getSurahDetail = async () => {
         try {
@@ -18,35 +21,64 @@ export default function SurahDetail() {
         }
     };
 
+    const getTranslation = async () => {
+        try {
+            const res = await fetch(`https://api.alquran.cloud/v1/surah/${id}/id.indonesian`);
+            const data = await res.json();
+            setTranslation(data.data.ayahs);
+        } catch (err) {
+            console.error("Gagal ambil terjemahan:", err);
+        }
+    };
+
     useEffect(() => {
         getSurahDetail();
+        getTranslation();
     }, [id]);
 
     if (!surah) {
         return <p className="p-4">Loading Dulu Gois...</p>;
     }
     return (
-        <div className="p-4">
-            <h1 className="text-xl font-bold">SurahDetail</h1>
-            <p>Selamat datang di aplikasi Al-Qur'an interaktif.</p>
-            <div className="p-4">
-                <h1 className="text-xl font-bold mb-2">
-                    {surah.englishName} ({surah.name})
-                </h1>
-                <p className="mb-2">
-                    Tempat Turun: {surah.revelationType === "Meccan" ? "Mekah" : "Madinah"}
-                </p>
-                <p className="mb-4">Jumlah Ayat: {surah.numberOfAyahs}</p>
+        <div className="bg-white">
+            <NavbarHeader bgClass="bg-sage-100" />
 
-                <ul className="space-y-3">
-                    {surah.ayahs.map((ayah) => (
-                        <li key={ayah.number} className="border-b pb-2">
-                            <p className="font-arabic text-right text-2xl">{ayah.text}</p>
-                            <p className="text-sm text-gray-500">Ayat {ayah.numberInSurah}</p>
-                        </li>
-                    ))}
-                </ul>
+            <div className="pt-30 mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:mb-10 space-y-7">
+                <h1 className="text-xl font-bold">SurahDetail</h1>
+                <p>Selamat datang di aplikasi Al-Qur'an interaktif.</p>
+                <div className="p-4">
+                    <h1 className="text-xl font-bold mb-2">
+                        {surah.englishName} ({surah.name})
+                    </h1>
+                    <p className="mb-2">
+                        Tempat Turun: {surah.revelationType === "Meccan" ? "Mekah" : "Madinah"}
+                    </p>
+                    <p className="mb-4">Jumlah Ayat: {surah.numberOfAyahs}</p>
+
+                    <ul className="space-y-3">
+                        {surah.ayahs.map((ayah, index) => {
+                            const isFirstAyah = ayah.numberInSurah === 1;
+                            const isAlFatihah = surah.number === 1;
+                            const bismillah = "بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ";
+
+                            const arabText = !isAlFatihah && isFirstAyah && ayah.text.includes(bismillah)
+                                ? ayah.text.replace(bismillah, "").trimStart()
+                                : ayah.text;
+
+                            const translatedText = translation[index]?.text ?? "";
+
+                            return (
+                                <li key={ayah.number} className="border-b pb-2 space-y-1">
+                                    <p className="font-arabic text-right text-2xl">{arabText}</p>
+                                    <p className="text-sm text-gray-500 italic">{translatedText}</p>
+                                    <p className="text-xs text-gray-400">Ayat {ayah.numberInSurah}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
             </div>
+            <Footer />
         </div>
     );
 }
