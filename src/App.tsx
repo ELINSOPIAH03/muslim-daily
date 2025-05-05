c9c13a325820be758a3fd19632afcdee4e80aeee
Elin Sopiah <gitgit367@gmail.com>
2025-05-05 18:51:09 +0700

    feat:  add base feedback layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
 const Home = lazy(() => import('./pages/Home'));
+const Feedback = lazy(() => import('./pages/Feedback'));
 const AlQuran = lazy(() => import('./pages/Alquran/Index'));
 const AlMatsurat = lazy(() => import('./pages/Almatsurat/Index'));
 const DzikirPagi = lazy(() => import('./pages/Almatsurat/Pagi'));
@@ -16,6 +17,7 @@ function App() {
       <Suspense fallback={<div>Loading Dulu Gois...</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/feedback" element={<Feedback />} />
         <Route path="/al-quran" element={<AlQuran />} />
         <Route path="/al-matsurat" element={<AlMatsurat />} />
         <Route path="/al-matsurat/pagi" element={<DzikirPagi />} />
---
+++ src/pages/Feedback.tsx
@@ -0,0 +1,12 @@
+import NavbarHeader from "../components/NavbarHeader"
+import Footer from "../components/Footer"
+
+export default function Feedback() {
+    return (
+        <div className="bg-white">
+            <NavbarHeader bgClass="bg-sage-100" />
+
+            <Footer />
+        </div>
+    )
+}
